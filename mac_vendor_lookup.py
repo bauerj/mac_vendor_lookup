09716e700467402b6b2f690c7e4c1a493a9b08c6
jiahchen <jianhaoch@gmail.com>
2020-10-30 07:03:33 +0000

    handle exception for unrecognized vendor mac

--- mac_vendor_lookup.py
+++ mac_vendor_lookup.py
@@ -91,7 +91,11 @@ class AsyncMacLookup(BaseMacLookup):
             await self.load_vendors()
         if type(mac) == str:
             mac = mac.encode("utf8")
-        return self.prefixes[mac[:6]].decode("utf8")
+
+        if mac[:6] in self.prefixes.keys():
+            return self.prefixes[mac[:6]].decode("utf8")
+        else:
+            return 'Unrecognized Vendor'
 
 
 class MacLookup(BaseMacLookup):
